53fb7d539827a358a3c90024229d67f920f7a84d
Ivan Katkov <vanya6537@gmail.com>
2022-12-12 06:39:16 +0700

    remove OPENAI_API_KEY from ecosystem conf

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{  name: "back",
     script: "yarn start",
     env: {
-      OPENAI_API_KEY: "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"
+      OPENAI_API_KEY: "yourkey"
     },
 },{  name: "front",
     script: "cd ../-your-image-react-metamask && yarn dev",
